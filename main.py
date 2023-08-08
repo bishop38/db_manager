8f7728aaef9da1a834ec2420e5f8b5d6fdd9edf1
bishop38 <ubica33@mail.ru>
2023-08-08 14:08:08 +0600

    done

--- main.py
+++ main.py
@@ -27,74 +27,80 @@ def create_tables(cursor):
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tasks(
         id INT PRIMARY KEY,
-        user_id INT,
+        username INT,
         project_id INT,
         name TEXT,
         type TEXT,
         description TEXT);
     """)
 
+class SqlRequests:
+    table_name = None
+    columns = []
+    cursor = None
+    connect = None
 
-def create_projects(cursor, name):
-    cursor.execute(f"""
-        INSERT INTO projects( name) 
-        VALUES({name});
-    """)
+    def __init__(self, table_name, columns, connect, cursor):
+        self.table_name = table_name
+        self.columns = columns
+        self.connect = connect
+        self.cursor = cursor
 
+    def create_record(self):
+        values = [input(f'Введите значение для {key}: ') for key in self.columns]
+        self.cursor.execute(f"""
+            INSERT INTO {self.table_name} ({','.join(self.columns)}) 
+            VALUES({", ".join(['?' for i in values])});
+        """, values)
+        self.connect.commit()
+        self.select_record()
 
-def create_users(cursor, connect):
-    username = input('Введите username: ')
-    fname = input('Введите имя: ')
-    lname = input('Введите фамилию: ')
-    gender = input('Введите пол: ')
-    cursor.execute(f"""
-        INSERT INTO users(username, fname, lname, gender) 
-        VALUES(?, ?, ?, ?);
-    """, (username, fname, lname, gender))
-    connect.commit()
+    def delete_record(self):
+        username = input('Введите username: ')
+        self.cursor.execute(f"DELETE FROM users WHERE username = '{username}';")
+        self.connect.commit()
+        self.select_record()
 
+    def update_record(self):
+        identifier_field = input(f'Введите поле идентификатор ({self.columns}): ')
+        identifier_value = input('Введите текущее значение идентификатора: ')
+        values = {}
+        print('Доступные поля: ', ', '.join(self.columns))
+        while True:
+            field = input('Введите поле для изменения(пустое поле, если хотите сохранить изменения): ')
+            if not field:
+                break
+            value = input('Введите значение: ')
+            values[field] = value
 
-def delete_users(cursor, connect):
-    username = input('Введите username: ')
-    cursor.execute(f"DELETE FROM users WHERE username = '{username}';")
-    connect.commit()
-    select_users(cursor, connect)
+        self.cursor.execute(f"""
+                UPDATE {self.table_name}
+                SET {', '.join([key + '=' + f"'{value}'" for key, value in values.items()])}
+                WHERE {identifier_field} = '{identifier_value}';
+            """)
+        self.connect.commit()
+        self.select_record()
 
-
-def update_users(cursor, connect):
-    field_to_change = input('Введите поле для изменения: ')
-    change_from = input('Введите старое значение: ')
-    change_to = input('Введите новое значение: ')
-    cursor.execute(f"""
-            UPDATE users
-            SET '{field_to_change}' = '{change_to}'
-                'gender'='[[[['
-            WHERE '{field_to_change}' = '{change_from}';
+    def select_record(self):
+        self.cursor.execute(f"""
+            SELECT * FROM {self.table_name}; 
         """)
-    connect.commit()
-    select_users(cursor, connect)
+        print(self.cursor.fetchall())
 
-
-def select_users(cursor, connect):
-    cursor.execute(f"""
-        SELECT * FROM users; 
-    """)
-    print(cursor.fetchall())
-
-
-def filter_users(cursor, connect):
-    fname = input('Введите имя: ')
-    # lname = input('Введите фамилию: ')
-    cursor.execute(f"""
-        SELECT * FROM users WHERE fname='{fname}'
-    """)
-
-
-def create_tasks(cursor, name):
-    cursor.execute(f"""
-        INSERT INTO projects(name) 
-        VALUES({name});
-    """)
+    def filter_record(self):
+        values = {}
+        print('Доступные поля: ', ', '.join(self.columns))
+        while True:
+            field = input('Введите поле для фильтрации(пустое поле - получение результатов): ')
+            if not field:
+                break
+            value = input('Введите значение: ')
+            values[field] = value
+        self.cursor.execute(f"""
+            SELECT * FROM {self.table_name} 
+            WHERE {', '.join([key + '=' + f"'{value}'" for key, value in values.items()])}
+        """)
+        print(self.cursor.fetchall())
 
 
 def main():
@@ -104,26 +110,38 @@ def main():
         create_tables(cur)
         conn.commit()
 
+    user_request = SqlRequests('users', ['username', 'fname', 'lname', 'gender'], conn, cur)
+    project_request = SqlRequests('projects', ['id', 'name'], conn, cur)
+    task_request = SqlRequests('tasks', ['id', 'username', 'project_id', 'name', 'type', 'description'], conn, cur)
+
     user_actions = {
         'user': {
-            'create': create_users,
-            'update': update_users,
-            'delete': delete_users,
-            'select': select_users,
-            'filter': filter_users,
+            'create': user_request.create_record,
+            'update': user_request.update_record,
+            'delete': user_request.delete_record,
+            'select': user_request.select_record,
+            'filter': user_request.filter_record,
         },
         'project': {
-
+            'create': project_request.create_record,
+            'update': project_request.update_record,
+            'delete': project_request.delete_record,
+            'select': project_request.select_record,
+            'filter': project_request.filter_record,
         },
         'task': {
-
+            'create': task_request.create_record,
+            'update': task_request.update_record,
+            'delete': task_request.delete_record,
+            'select': task_request.select_record,
+            'filter': task_request.filter_record,
         }
     }
     while True:
         table_input = input("Выберите таблицу(user, project, task): ")
         table_action_input = input("Выберите операцию(create, delete, update, select(все записи), filter): ")
         action = user_actions.get(table_input).get(table_action_input)
-        action(cur, conn)
+        action()
 
 
 if __name__ == '__main__':
